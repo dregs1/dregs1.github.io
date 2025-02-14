893bcff273aa4afec7a7ff894ba6554afc4d403c
Dregs <black1977eagle@gmail.com>
2025-02-14 21:34:28 +0000

    OK

--- omega/plugin.video.eagle.blvck/service_entry.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Service entry point"""
-
-#from __future__ import absolute_import, division, unicode_literals
-from lib.loggers import Logger
-if __name__ == '__main__':
-    from lib.service import run
-
-    run()

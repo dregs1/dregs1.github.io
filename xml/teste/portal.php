7fbf9d974d4215411bc9eb2da9f248467a584891
Dregs <black1977eagle@gmail.com>
2025-09-14 01:40:07 +0100

    ok

--- xml/teste/portal.php
+++ xml/teste/portal.php
@@ -1,12 +1,14 @@
 <?php
-// Define o tipo de conteúdo para texto simples.
-// Isso garante que o aplicativo receba apenas o texto puro do DNS, sem HTML ou outros dados.
-header('Content-Type: text/plain');
+// Define o tipo de conteúdo como JSON.
+header('Content-Type: application/json');
 
-// Este é o endereço de DNS que você quer que o aplicativo use.
-// Você pode modificar este valor sempre que precisar.
+// O endereço de DNS que você quer usar.
 $dns_address = "http://repi.live:8080/c/";
 
-// Imprime o endereço do DNS na resposta HTTP.
-echo $dns_address;
+// Cria um array associativo para a resposta.
+// Use "url" como a chave se for o que o aplicativo espera.
+$response = array("url" => $dns_address);
+
+// Converte o array em uma string JSON e a imprime.
+echo json_encode($response);
 ?>
